cb66767a8e55f98e5027e3b4a2168466e8fc2d0f
Denys Zosimovych <unsinedz.work@gmail.com>
2019-12-24 09:24:30 +0200

    Added bindings init

--- zstu/lib/main.dart
+++ zstu/lib/main.dart
@@ -2,6 +2,7 @@ import 'package:flutter/widgets.dart';
 import 'package:zstu/Application.dart';
 
 void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
   var app = new ZstuApp();
   await app.initialize();
   runApp(app);
